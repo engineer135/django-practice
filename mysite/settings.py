b07d655f395fd5415353af223a1cd4d38dfbc960
engineer135 <everafterk@naver.com>
2018-03-14 16:30:20 +0900

    Django 관리자 두둥

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko' #한글로 변경
 
 TIME_ZONE = 'Asia/Seoul'
 
